1aa2afb69a6e4f3fe95365c152adbd8e01e46a49
Lich <longzy2013@gmail.com>
2015-03-01 15:16:18 +0800

    ok

--- Application/Admin/Controller/BaseController.class.php
+++ Application/Admin/Controller/BaseController.class.php
@@ -1,32 +1,14 @@
-<<<<<<< HEAD
 <?php
 namespace Admin\Controller;
 use Think\Controller;
-class BaseController extends Controller {
+class BaseController extends Controller
+{
     public function _initialize(){
-        if(session('user.uid1')==null || session('user.name1')==null){
+        if (session('user.uid1') == null || session('user.name1') == null) {
             $this->error('请先登录', U('Login/index'));
-        }
-        else{
+        } else {
             $username = session('user.name1');
             $this->assign('username', $username);
         }
     }
-   
-=======
-<?php
-namespace Admin\Controller;
-use Think\Controller;
-class BaseController extends Controller {
-    public function _initialize(){
-        if(session('user.uid1')==null || session('user.name1')==null){
-            $this->error('请先登录', U('Login/index'));
-        }
-        else{
-            $username = session('user.name');
-            $this->assign('username', $username);
-        }
-    }
-   
->>>>>>> 05142212eed88e15509f156e3da4c15b452401e2
 }
