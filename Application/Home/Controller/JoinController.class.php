c2922a9f62282395fc6fcbad7f260ffcfa573ed4
Lich <longzy2013@gmail.com>
2015-03-14 20:44:14 +0800

    join textarea

--- Application/Home/Controller/JoinController.class.php
+++ Application/Home/Controller/JoinController.class.php
@@ -116,6 +116,7 @@ class JoinController extends BaseController {
         }
         $store_img = $img_url[0].'?imageView2/5/w/121/h/121';
         $person_img = $img_url[1].'?imageView2/2/w/363/h/363';
+        $data['person_introduce'] = str_replace(array("\r\n",'\r','\n'), '<br/>',$data['person_introduce']);
         $user = M('users');
         $store = M('store');
         $tag = M('tags');
