b3bfa099aeb5ba15c717f9482442293c41a61ea1
Ling <wssgcg1213@qq.com>
2015-03-18 18:43:05 +0800

    fix

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -13,9 +13,9 @@ return array(
     */
     'DB_TYPE'               =>  'mysql',     // 数据库类型
     'DB_HOST'               =>  'localhost', // 服务器地址
-    'DB_NAME'               =>  'microstore', // 数据库名
-    'DB_USER'               =>  'root',      // 用户名
-    'DB_PWD'                =>  '',    // 密码
+    'DB_NAME'               =>  'wssgcg1213_microstore', // 数据库名
+    'DB_USER'               =>  'wssgcg1213',      // 用户名
+    'DB_PWD'                =>  '6884650',    // 密码
     'DB_PORT'               =>  '3306',     // 端口
     //'DB_PREFIX'             =>  'hy_',    	// 数据库表前缀
     'DB_FIELDTYPE_CHECK'    =>  false,       // 是否进行字段类型检查
