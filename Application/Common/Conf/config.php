d24cd8179a6a6374b420e44aff1e31037cca0a5f
Ling <wssgcg1213@qq.com>
2015-03-18 18:19:57 +0800

    fix

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -4,7 +4,7 @@ return array(
     //'SHOW_PAGE_TRACE' =>true,
     'URL_MODEL'=>1,
     //显示调试日志
-    //'SHOW_PAGE_TRACE'=>true,
+	//'SHOW_PAGE_TRACE'=>true,
     //路由对大小写不敏感
     'URL_CASE_INSENSITIVE'=>true,
 
@@ -13,9 +13,9 @@ return array(
     */
     'DB_TYPE'               =>  'mysql',     // 数据库类型
     'DB_HOST'               =>  'localhost', // 服务器地址
-    'DB_NAME'               =>  'microstore', // 数据库名
-    'DB_USER'               =>  'root',      // 用户名
-    'DB_PWD'                =>  '',    // 密码
+    'DB_NAME'               =>  'wssgcg1213_microstore', // 数据库名
+    'DB_USER'               =>  'wssgcg1213',      // 用户名
+    'DB_PWD'                =>  '6884650',    // 密码
     'DB_PORT'               =>  '3306',     // 端口
     //'DB_PREFIX'             =>  'hy_',    	// 数据库表前缀
     'DB_FIELDTYPE_CHECK'    =>  false,       // 是否进行字段类型检查
