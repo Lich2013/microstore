fa9b221bb3395f8d543a0b2ebd2dfa7e40464b7d
Lich <longzy2013@gmail.com>
2015-04-13 21:23:15 +0800

    db

---
+++ Application/Common/Conf/config.php
@@ -0,0 +1,24 @@
+<?php
+return array(
+	//'配置项'=>'配置值'
+    //'SHOW_PAGE_TRACE' =>true,
+    'URL_MODEL'=>1,
+    //显示调试日志
+	//'SHOW_PAGE_TRACE'=>true,
+    //路由对大小写不敏感
+    'URL_CASE_INSENSITIVE'=>true,
+
+    /*
+     * 数据库配置
+    */
+    'DB_TYPE'               =>  'mysql',     // 数据库类型
+    'DB_HOST'               =>  '10.4.12.173', // 服务器地址
+    'DB_NAME'               =>  'dca78f378ea6142c3bdf4ac6867ec3771', // 数据库名
+    'DB_USER'               =>  'uFCUlN0PVA9C2',      // 用户名
+    'DB_PWD'                =>  'pDxdRC9ZZpMuO',    // 密码
+    'DB_PORT'               =>  '3306',     // 端口
+    //'DB_PREFIX'             =>  'hy_',    	// 数据库表前缀
+    'DB_FIELDTYPE_CHECK'    =>  false,       // 是否进行字段类型检查
+    'DB_FIELDS_CACHE'       =>  false,        // 启用字段缓存
+    'DB_CHARSET'            =>  'utf8',
+);
