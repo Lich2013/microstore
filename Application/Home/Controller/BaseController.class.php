e857fa0d9f657c72126f53b7d22278b7d84c6331
Lich <longzy2013@gmail.com>
2015-04-18 15:23:36 +0800

    user help

--- Application/Home/Controller/BaseController.class.php
+++ Application/Home/Controller/BaseController.class.php
@@ -6,6 +6,8 @@ class BaseController extends Controller {
         $openId = I('get.openid');
         session('openId', $openId);
         $topic = M('topic')->order('time desc')->limit(4)->field('id, pic')->select();
+        $help = M('topic')->order('id asc')->find();
         $this->assign('topic', $topic);
+        $this->assign('help', $help);
     }
 }
